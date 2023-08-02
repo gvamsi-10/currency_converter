618d34d8e9826e775748ee23eb4f891631c56d0c
Vamsi <109903812+gvamsi-10@users.noreply.github.com>
2023-08-02 22:55:33 +0530

    Add files via upload

---
+++ currency-converter/app.py
@@ -0,0 +1,30 @@
+from flask import Flask,request,jsonify
+import requests
+import streamlit as st
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def index():
+    data = request.get_json()
+    source_currency = data['queryResult']['parameters']['unit-currency']['currency']
+    amount = data['queryResult']['parameters']['unit-currency']['amount']
+    target_currency = data['queryResult']['parameters']['currency-name']
+
+
+    cf = fetch_conversion_factor(source_currency,target_currency)
+    final_amount = amount * cf
+    final_amount = round(final_amount,2)
+    response = {
+        'fulfillmentText': "{} {} is {} {}".format(amount,source_currency,final_amount,target_currency)
+    }
+    print(final_amount)
+    return jsonify(response)
+
+def fetch_conversion_factor(source,target):
+    url = "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_oBmJBO9usRezGVW6rMvKTj0hDXe668HXeXcof7xX&currencies={}&base_currency={}".format(target,source)
+    response = requests.get(url)
+    response = response.json()
+    return response['data']['{}'.format(target)]
+
+if __name__ == "__main__":
+    app.run(debug=True)
